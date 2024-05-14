9859fab715318730e8d4a9f7a977ec8f4efa3ab8
Abdo <ba2662003@gmail.com>
2024-05-15 02:40:48 +0300

    some improvment

--- src/components/AddToWishlist.jsx
+++ src/components/AddToWishlist.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prop-types */
 import { IoHeart, IoHeartOutline } from "react-icons/io5";
-import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { addFavourite, removeFavourite } from "../redux/FavouriteSlice";
 import { useState } from "react";
+import { addToWishlistFn } from "../functions/addToWishlistFn";
 
 export default function AddToWishlist({ prodId }) {
   const [load, setLoad] = useState(false);
@@ -11,39 +10,11 @@ export default function AddToWishlist({ prodId }) {
   const favourite = useSelector(state=>state.favourite);
   const favor = favourite.find(ele=>ele._id === prodId)
   // console.log(favorite)
-  const addToWishlistFn = () => {
-    // console.log("clicked");
-    setLoad(true);
-    if(favor){
-      axios.delete(`https://ecommerce-api-hlp7.onrender.com/api/wishlist/${prodId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.token}`,
-          },
-        }
-      ).then(()=>dispatch(removeFavourite(prodId))).finally(()=>setLoad(false))
-    }
-    else{
-      axios
-        .post(
-          "https://ecommerce-api-hlp7.onrender.com/api/wishlist",
-          { product: prodId },
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.token}`,
-            },
-          }
-        )
-        .then((res) => {
-          console.log(res)
-          dispatch(addFavourite(res.data.data))
-      }).catch((e) => console.log(e)).finally(()=>setLoad(false))
-    }
-  };
+  
   return (
     <button
       className="absolute top-1 right-1 shadow-lg bg-white p-2 text-red-600  text-2xl rounded-full"
-      onClick={addToWishlistFn}
+      onClick={()=>addToWishlistFn(prodId,favor,dispatch,setLoad)}
       disabled={load}
     >
       {favor ? <><IoHeart /></> : <><IoHeartOutline/></>}
---
+++ src/functions/addToWishlistFn.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { addFavourite, removeFavourite } from "../redux/FavouriteSlice";
+
+export const addToWishlistFn = (prodId,favor,dispatch,setLoad) => {
+    // console.log("clicked");
+    setLoad(true);
+    if(favor){
+        axios.delete(`https://ecommerce-api-hlp7.onrender.com/api/wishlist/${prodId}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.token}`,
+                },
+            }
+        ).then(()=>dispatch(removeFavourite(prodId))).finally(()=>setLoad(false))
+    }
+    else{
+        axios.post("https://ecommerce-api-hlp7.onrender.com/api/wishlist",
+            { product: prodId },
+            {
+                headers: {
+                Authorization: `Bearer ${localStorage.token}`,
+                },
+            }
+        ).then((res) => {
+            // console.log(res)
+            dispatch(addFavourite(res.data.data))
+        }).catch((e) => console.log(e)).finally(()=>setLoad(false))
+    }
+};
