42854d9e672d6f570c3da66522c871dd0d228045
MohmedAref31 <mohmedwork2003@gmail.com>
2024-05-16 14:24:53 +0300

    add vil status

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,37 +6,37 @@ import AddToWishlist from "../components/AddToWishlist";
 import AddToCart from "./AddToCart";
 import { Link } from "react-router-dom";
 
-export default function ProductCard({ele}) {
-
+export default function ProductCard({ ele }) {
   return (
     <div
-    key={ele._id}
-    className=" relative pb-2 text-center bg-gray-100 rounded-md shadow-lg"
-  >
-    <AddToWishlist prodId={ele._id}/>
-    <Link to={`/bookDetails/${ele._id}`}>
-      <img
-        src={ele.imageCover || defaultImage}
-        alt={ele.title}
-        className="w-full rounded-t-md"
-      />
-    </Link>
-    <div className="px-3">
-      <h1 className="text-lg font-bold">
-        {ele.title.slice(0, 10)}...
-      </h1>
-      <p className="flex items-center justify-center gap-1 text-lg font-bold text-yellow-400">
-        {ele.ratingsAverage.toFixed(1)}{" "}
-        <FaStar className="text-sm" />
-      </p>
-      {/* <p className="my-2 text-sm font-bold text-gray-500 break-words">{ele.description.slice(0,80)}</p> */}
-      <div className="flex items-center justify-between px-2">
-        <p className="font-bold text-red-500">${ele.price}</p>
-        <AddToCart
-          ele={ele}
+      key={ele._id}
+      className=" relative pb-2 text-center bg-gray-100 rounded-md shadow-lg"
+    >
+      <AddToWishlist prodId={ele._id} />
+      <Link to={`/bookDetails/${ele._id}`}>
+        <img
+          src={ele.imageCover || defaultImage}
+          alt={ele.title}
+          className="w-full rounded-t-md"
         />
+      </Link>
+      <div className="px-3">
+        <h1 className="text-lg font-bold">{ele.title.slice(0, 10)}...</h1>
+        <div className="flex justify-between">
+          <p className="flex items-center justify-center gap-1 text-lg font-bold text-yellow-400">
+            {ele.ratingsAverage.toFixed(1)} <FaStar className="text-sm" />
+          </p>
+          <p className="text-sm font-semibold">
+            {ele.quantity > 0 ? `avl ${ele.quantity}`: `will be avil soon`}
+          </p>
+        </div>
+
+        {/* <p className="my-2 text-sm font-bold text-gray-500 break-words">{ele.description.slice(0,80)}</p> */}
+        <div className="flex items-center justify-between px-2">
+          <p className="font-bold text-red-500">${ele.price}</p>
+          <AddToCart ele={ele} />
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
