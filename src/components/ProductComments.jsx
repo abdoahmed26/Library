d23d29937d008882adde3408997ab6bcfc9ee381
MohmedAref31 <mohmedwork2003@gmail.com>
2024-05-16 15:26:36 +0300

    fix comments section

--- src/components/ProductComments.jsx
+++ src/components/ProductComments.jsx
@@ -12,66 +12,69 @@ export default function ProductComments() {
   //   console.log(id);
   const { data, isLoading, isError } = useGetProcutReviewsQuery(id);
   console.log(data, isLoading, isError);
-  const [comment,setComment] = useState(null)
+  const [comment, setComment] = useState(null);
   const reviews = data?.data?.map((r) => {
-    return (
-      <Comment
-        key={r._id}
-        comment={r}
-        setComment={setComment}
-      />
-    );
+    return <Comment key={r._id} comment={r} setComment={setComment} />;
   });
 
   console.log(reviews);
 
-  
   return (
     <div>
       {" "}
-      {
-        comment? (
-          <AddComment id={id} comment={comment} />
-        ) : (
-          <AddComment id={id} />
-        )
-      }
+      {comment ? (
+        <AddComment id={id} comment={comment} />
+      ) : (
+        <AddComment id={id} />
+      )}
       {data?.result === 0 ? <p>be the firest reviewer</p> : reviews}
     </div>
   );
 }
 
-const Comment = ({ comment,setComment }) => {
-  const user = useSelector(state=>state.user)
-  let dataTime = new Date(comment.createdAt).toLocaleDateString() + ' - ' +new Date(comment.createdAt).toLocaleTimeString();
-  console.log(dataTime);
+const Comment = ({ comment, setComment }) => {
+  const user = useSelector((state) => state.user);
+  const [imgSrc, setImgSrc] = useState(
+    comment.user.profileImage || defaultUserImg
+  );
+  
+  let dataTime =
+    new Date(comment.createdAt).toLocaleDateString() +
+    " - " +
+    new Date(comment.createdAt).toLocaleTimeString();
+  console.log(comment.user);
   return (
-    <div className="flex flex-col gap-3 sm:flex-row sm:justify-between items-center border my-3 rounded shadow-lg p-2">
+    <div className="relative flex gap-3 sm:flex-row sm:justify-between items-center border my-3 rounded shadow-lg p-2">
       <div className="flex gap-3">
         <div>
           <img
-            src={comment.user.profileImage || defaultUserImg}
+            src={imgSrc}
             alt="user reviewer image"
-            className="w-9 rounded-full border"
+            onError={() => setImgSrc(defaultUserImg)}
+            className="w-9 aspect-square rounded-full border"
           />
         </div>
         <div>
           <div>
-          <p className="font-semibold">{comment.user.username || comment.user.name}</p>
-          <p className="text-[11px] font-semibold text-gray-500">{dataTime}</p>
+            <p className="font-semibold">
+              {comment.user.username || comment.user.name}
+            </p>
+            <p className="text-[11px] font-semibold text-gray-500">
+              {dataTime}
+            </p>
           </div>
           <p className="flex items-center gap-1 text-xl font-bold text-yellow-400">
             {comment.rate} <FaStar className="text-lg" />
           </p>
-          {comment.title && <p className="font-semibold text-gray-600">{comment.title}</p>}
+          {comment.title && (
+            <p className="font-semibold text-gray-600">{comment.title}</p>
+          )}
         </div>
       </div>
       <div>
-        {
-          comment.user._id ===user._id?
-          <UpdateDeleteComment comment={comment} setComment={setComment}/>
-          :null
-        }
+        {comment.user._id === user._id ? (
+          <UpdateDeleteComment comment={comment} setComment={setComment} />
+        ) : null}
       </div>
     </div>
   );
--- src/components/Update&DeleteComment.jsx
+++ src/components/Update&DeleteComment.jsx
@@ -1,39 +1,53 @@
 /* eslint-disable react/prop-types */
 import axios from "axios";
 import { useState } from "react";
+import { GoKebabHorizontal } from "react-icons/go";
 
-
-const UpdateDeleteComment = ({comment,setComment}) => {
-    const [load,setLoad] = useState(false);
-    const deleteComment = (ele)=>{
-        setLoad(true);
-        axios.delete(`https://ecommerce-api-hlp7.onrender.com/api/review/${ele._id}`,{
-            headers:{
-                Authorization:`Bearer ${localStorage.token}`
-            }
-        }).then(res=>console.log(res)).finally(()=>window.location.reload())
-    }
-    const update = (ele)=>{
-        setComment({...ele})
-    }
-    return (
-        <div>
-            <div className="flex gap-3">
-                <button onClick={()=>update(comment)}
-                className="bg-blue-500 text-white p-1 px-2 rounded">Update</button>
-                <button onClick={()=>deleteComment(comment)} disabled={load}
-                className="bg-red-500 text-white p-1 px-2 rounded">
-                    {
-                        load?
-                            <div className="flex justify-center">
-                                <span className="inline-block w-5 h-5 border-2 border-white rounded-full border-l-gray-500 animate-spin"></span>
-                            </div>
-                        :"Delete"
-                    }
-                </button>
-            </div>
-        </div>
-    );
-}
+const UpdateDeleteComment = ({ comment, setComment }) => {
+  const [load, setLoad] = useState(false);
+  const [isOpen , setOpen] = useState(false);
+  const deleteComment = (ele) => {
+    setLoad(true);
+    axios
+      .delete(`https://ecommerce-api-hlp7.onrender.com/api/review/${ele._id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.token}`,
+        },
+      })
+      .then((res) => console.log(res))
+      .finally(() => window.location.reload());
+  };
+  const update = (ele) => {
+    setComment({ ...ele });
+  };
+  return (
+    <div className="w-24 absolute top-1 right-1 ">
+      <div className="flex flex-col">
+        <button onClick={()=>setOpen(!isOpen)} className="w-7 h-7  rounded-full text-center block ml-auto active:bg-gray-200 ">
+          <GoKebabHorizontal />
+        </button>
+        {isOpen && <div className="text-center font-semibold text-sm shadow-lg">
+          <button onClick={() => update(comment)} className="p-1 px-2 border-b">
+            Update
+          </button>
+          <button
+            onClick={() => deleteComment(comment)}
+            disabled={load}
+            className="p-1 px-2"
+          >
+            {load ? (
+              <div className="flex justify-center">
+                <span className="inline-block w-5 h-5 border-2 border-white rounded-full border-l-gray-500 animate-spin"></span>
+              </div>
+            ) : (
+              "Delete"
+            )}
+          </button>
+        </div>}
+        
+      </div>
+    </div>
+  );
+};
 
 export default UpdateDeleteComment;
