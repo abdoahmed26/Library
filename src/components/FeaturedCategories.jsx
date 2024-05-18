3d888a1a6c68fb256fc6942e6f05989d1f1ed0ba
MohmedAref31 <mohmedwork2003@gmail.com>
2024-05-18 08:00:33 +0300

    add colors to catoegories

--- src/components/FeaturedCategories.jsx
+++ src/components/FeaturedCategories.jsx
@@ -35,8 +35,10 @@ export default function FeaturedCategories() {
 
 // eslint-disable-next-line react/prop-types
 const CategoryCard = ({ title, image,id }) => {
+  const colors = ["bg-red-100", "bg-lime-100","bg-green-100", "bg-orange-100","bg-blue-100", "bg-purple-100"]
+
   return (
-    <div className="p-3 text-center rounded-lg bg-slate-100">
+    <div className={`p-3 text-center rounded-lg ${colors[Math.floor((Math.random() * colors.length))]}`}>
       <img
         src={image || defaultImage}
         alt="category image"
